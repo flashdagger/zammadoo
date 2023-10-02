d8d1cf29700b62db40ae68b4a118ee5609fe5a5e
Marcel <flashdagger@googlemail.com>
2023-10-02 05:40:55 +0200

    add groups.pyi

---
+++ zammadoo/groups.pyi
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from typing import List
+
+from .resource import UpdatableResource
+from .resources import SearchableT
+from .users import User
+
+class Group(UpdatableResource):
+    active: bool
+    name: str
+    note: str
+    shared_drafts: bool
+    users: List[User]
+
+class Groups(SearchableT[Group]):
+    RESOURCE_TYPE = Group
