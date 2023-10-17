466f445059ac1065da5128448d24d8a96d48012c
Marcel <flashdagger@googlemail.com>
2023-10-17 02:00:06 +0200

    refactor recording file format

--- conftest.py
+++ conftest.py
@@ -3,8 +3,9 @@
 
 import os.path
 from http.client import responses
+from mmap import mmap, ACCESS_READ
 from pathlib import Path
-from typing import Any, Dict, Generator
+from typing import Any, Dict, Generator, Tuple
 from unittest.mock import patch
 from urllib.parse import urlencode
 
@@ -46,7 +47,7 @@ def client(request) -> Client:
     http_token = request.config.getoption("--http-token")
 
     if os.path.exists(http_token):
-        with open(http_token) as fd:
+        with open(http_token, "utf-8") as fd:
             http_token = fd.read()
 
     client = Client(client_url, http_token=http_token)
@@ -54,43 +55,46 @@ def client(request) -> Client:
     return client
 
 
-class FileDict:
+class FileWriter:
     def __init__(self, path: Path) -> None:
-        self.fd = open(path, "a+")
-        self.fd.seek(0)  # jump to beginning of file
-        self.line = 0
+        self.fd = open(path, "ab")
 
-    def __setitem__(self, key: str, value: str) -> None:
+    def append(self, key: str, data: bytes) -> None:
+        assert "\0" not in key
         fd = self.fd
-        fd.seek(0, 2)  # jump to end of file
-        print(key, value, file=fd)
-        self.line = 0
-
-    def __getitem__(self, key: str) -> str:
-        fd = self.fd
-        wrapped = False
-        last_line = self.line
-
-        while True:
-            for line in fd:
-                if wrapped and self.line == last_line:
-                    break
-
-                self.line += 1
-                if line.startswith(key):
-                    return line[len(key) + 1 : -1]
-
-            if wrapped:
-                break
-
-            fd.seek(0)
-            self.line = 0
-            wrapped = True
-
-        raise KeyError(key)
+        fd.writelines((f"{key}\0{len(data)}\n".encode("utf-8"), data, b"\n"))
 
     def clear(self) -> None:
         self.fd.truncate(0)
+
+    def close(self) -> None:
+        self.fd.close()
+
+
+class FileReader:
+    def __init__(self, path: Path) -> None:
+        self.fd = fd = open(path, "rb")
+        mm = mmap(fd.fileno(), 0, access=ACCESS_READ)
+        self.index = self.build_index(mm)
+        mm.close()
+
+    @staticmethod
+    def build_index(mm) -> Dict[str, Tuple[int, int]]:
+        index: Dict[str, Tuple[int, int]] = {}
+
+        for line in iter(mm.readline, b""):
+            key, bdata_len = line.decode("utf-8").rsplit("\0", maxsplit=1)
+            data_len = int(bdata_len)
+            index[key] = (mm.tell(), data_len)
+            mm.seek(int(bdata_len) + 1, 1)
+
+        return index
+
+    def __getitem__(self, item: str) -> bytes:
+        offset, length = self.index[item]
+        fd = self.fd
+        fd.seek(offset)
+        return fd.read(length)
 
     def close(self) -> None:
         self.fd.close()
@@ -104,42 +108,49 @@ def rclient(request: pytest.FixtureRequest, client) -> Generator[Client, None, N
     rpath = request.path
     record_file = rpath.with_name(rpath.stem) / f"{request.function.__name__}.log"
 
-    if recording:
-        record_file.parent.mkdir(exist_ok=True)
-
-    recorder = FileDict(record_file)
-
-    if recording:
-        recorder.clear()
-
     def serialize(method: str, url: str, params: Dict[str, Any]) -> str:
         param_items = params.items() if isinstance(params, dict) else params or ()
         query = urlencode(sorted(param_items))
         join = "?" if query else ""
         return f"{method} {url}{join}{query}"
 
-    def replay_request(_self, method, url, *_args, **kwargs):
-        params = kwargs.pop("params", None)
-        key = serialize(method, url, params)
-        status, content = recorder[key].split(maxsplit=1)
-        response = Response()
-        status_code = int(status)
-        response.status_code = status_code
-        response.reason = responses[status_code]
-        response._content = content.encode()
+    if recording:
+        record_file.parent.mkdir(exist_ok=True)
+        recorder = FileWriter(record_file)
+        recorder.clear()
 
-        return response
+        def cleanup() -> None:
+            recorder.close()
 
-    def record_request(self: Session, method, url, *args, **kwargs):
-        params = kwargs.get("params", None)
-        response = session_request(self, method, url, *args, **kwargs)
-        key = serialize(method, url, params)
-        recorder[key] = f"{response.status_code} {response.text}"
-        return response
+        def _request(self: Session, method, url, *args, **kwargs):
+            params = kwargs.get("params", None)
+            response = session_request(self, method, url, *args, **kwargs)
+            key = serialize(method, url, params)
+            data = b"\0".join(
+                (bytes(str(response.status_code), "ascii"), response.content)
+            )
+            recorder.append(key, data)
+            return response
 
-    with patch.object(
-        Session, "request", new=record_request if recording else replay_request
-    ):
+    else:
+        replay = FileReader(record_file)
+
+        def cleanup() -> None:
+            replay.close()
+
+        def _request(self: Session, method, url, *args, **kwargs):
+            params = kwargs.pop("params", None)
+
+            response = Response()
+            key = serialize(method, url, params)
+            status, response._content = replay[key].split(b"\0", maxsplit=1)
+            status_code = int(status)
+            response.status_code = status_code
+            response.reason = responses[status_code]
+
+            return response
+
+    with patch.object(Session, "request", new=_request):
         yield client
 
-    recorder.close()
+    cleanup()
