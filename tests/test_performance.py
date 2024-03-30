b3984a0ae5fd295499e0838f01cb4fe707f8af4b
flashdagger <flashdagger@googlemail.com>
2024-03-30 10:20:11 +0100

    measure property performance
    Signed-off-by: flashdagger <flashdagger@googlemail.com>

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -13,14 +13,19 @@ number = int(1e5)
 def info_ticket() -> Ticket:
     client = Client("https://localhost/api/v1", http_token="myfaketoken")
     info = {"id": 123, "title": "some title", "created_at": "2024-03-18T22:43:07.089Z"}
-    return client.tickets(123, info=info)
+    yield client.tickets(123, info=info)
 
 
 @pytest.fixture(scope="module")
 def info_user() -> User:
+    def _myprop(self):
+        return self["login"]
+
     client = Client("https://localhost/api/v1", http_token="myfaketoken")
     info = {"id": 123, "login": "john.doe@pytest"}
-    return client.users(123, info=info)
+    user = client.users(123, info=info)
+    user.__class__.property = property(_myprop)
+    yield user
 
 
 def test_info_attribute(info_ticket, benchmark):
@@ -43,3 +48,7 @@ def test_info_datetime(info_ticket, benchmark):
 
 def test_info_descriptor(info_user, benchmark):
     benchmark(timeit, "user.name", number=number, globals={"user": info_user})
+
+
+def test_info_property(info_user, benchmark):
+    benchmark(timeit, "user.property", number=number, globals={"user": info_user})
